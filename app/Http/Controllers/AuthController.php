01a225d30ddf1d6bc1f7abf42785cbdd3d36f8f3
MhmdShamii <mhmddshamii@gmail.com>
2025-12-17 19:15:40 +0200

    create Register user endpoint, fix token generation, fix throttle

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
 
 class AuthController extends Controller
 {
-    //
+    function register(Request $request)
+    {
+        $validated = $request->validate([
+            'name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8|confirmed',
+        ]);
+
+        $hashedPassword = Hash::make($validated['password']);
+
+        $user = User::create([
+            'name' => $validated['name'],
+            'email' => $validated['email'],
+            'password' => $hashedPassword,
+        ]);
+
+        $token = $user->createToken('react-client')->plainTextToken;
+
+        return response()->json([
+            'user' => $user,
+            'access_token' => $token,
+        ], 201);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,8 @@ use Illuminate\Support\Facades\Route;
 
 //Authentication apis
 Route::controller(AuthController::class)->group(function () {
-    Route::post('/register', 'register')->throttle('10,1');
-    Route::post('/login', 'login')->throttle('10,1');
+    Route::post('/register', 'register')->middleware('throttle:10,1');
+    Route::post('/login', 'login')->middleware('throttle:10,1');
 
     Route::middleware('auth:sanctum')->group(function () {
         Route::get('/me', 'me');
