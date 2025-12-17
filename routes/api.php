c8fb585393f71e7e12447801c6caced0822caea2
MhmdShamii <mhmddshamii@gmail.com>
2025-12-17 18:12:55 +0200

    Grouping routes

--- routes/api.php
+++ routes/api.php
@@ -3,10 +3,13 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Support\Facades\Route;
 
-Route::post('/register', [AuthController::class, 'register']);
-Route::post('/login', [AuthController::class, 'login']);
+//Authentication apis
+Route::controller(AuthController::class)->group(function () {
+    Route::post('/register', 'register')->throttle('10,1');
+    Route::post('/login', 'login')->throttle('10,1');
 
-Route::middleware('auth:sanctum')->group(function () {
-    Route::Get('/me', [AuthController::class, 'logout']);
-    Route::post('/logout', [AuthController::class, 'logout']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::get('/me', 'me');
+        Route::post('/logout', 'logout');
+    });
 });
