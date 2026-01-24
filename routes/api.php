603f2138a3a01ad131a09703da1513db68372db5
MhmdShamii <mhmddshamii@gmail.com>
2026-01-24 17:10:09 +0200

    defining basic Auth Apis

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,19 @@
 <?php
 
+use App\Http\Controllers\AuthContoller;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+
+Route::prefix('v1')->group(function () {
+
+    Route::prefix('auth')->group(function () {
+        Route::post('/login', [AuthContoller::class, 'login']);
+        Route::post('/register', [AuthContoller::class, 'register']);
+
+        Route::middleware('auth:sanctum')->group(function () {
+            Route::get('/profile', [AuthContoller::class, 'profile']);
+            Route::post('/logout', [AuthContoller::class, 'logout']);
+        });
+    });
+});
