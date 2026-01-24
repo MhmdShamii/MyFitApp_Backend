a044993e4fc72d03e352ec230a8e22004a89d268
MhmdShamii <mhmddshamii@gmail.com>
2026-01-24 17:25:47 +0200

    name spac fix XD

---
+++ app/Http/Controllers/Auth/AuthController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers\Auth;
+
+use App\Http\Controllers\Controller;
+
+class AuthController extends Controller
+{
+    function login()
+    {
+        // login logic
+    }
+    function register()
+    {
+        // register logic
+    }
+    function me()
+    {
+        // profile logic
+    }
+    function logout()
+    {
+        // logout logic
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,18 +1,19 @@
 <?php
 
-use App\Http\controller\auth\AuthContoller;
+use App\Http\Controllers\Auth\AuthController;
+
 use Illuminate\Support\Facades\Route;
 
 
 Route::prefix('v1')->group(function () {
 
     Route::prefix('auth')->group(function () {
-        Route::post('/login', [AuthContoller::class, 'login']);
-        Route::post('/register', [AuthContoller::class, 'register']);
+        Route::post('/login', [AuthController::class, 'login']);
+        Route::post('/register', [AuthController::class, 'register']);
 
         Route::middleware('auth:sanctum')->group(function () {
-            Route::get('/profile', [AuthContoller::class, 'profile']);
-            Route::post('/logout', [AuthContoller::class, 'logout']);
+            Route::get('/me', [AuthController::class, 'me']);
+            Route::post('/logout', [AuthController::class, 'logout']);
         });
     });
 });
